9b3102485d507c384a164a5942c2890a5c9a3f78
Sohrab Hosen <105154857+577111@users.noreply.github.com>
2023-03-03 10:33:54 +0600

    main.js

---
+++ main.js
@@ -0,0 +1,38 @@
+var btn =document.querySelector('#submit');
+var text=document.querySelector('#text');
+
+var password= document.querySelector('#password');
+var data=document.querySelector('.data');
+
+
+text.addEventListener('change', function(e){
+
+localStorage.setItem('text',e.target.value);
+});
+
+
+
+
+password.addEventListener('change', function(e){
+  
+localStorage.setItem('password',e.target.value);
+
+});
+
+btn.addEventListener('click',function(e){
+  
+});
+
+var gtext=localStorage.getItem('text');
+
+var gpassword=localStorage.getItem('password');
+
+let passwords=[];
+passwords.push(gpassword);
+localStorage.setItem('arps',passwords);
+var spass=localStorage.getItem('arps');
+var sub=document.querySelector('form');
+
+sub.addEventListener('submit',(e)=>{
+  e.preventDefault();
+})
